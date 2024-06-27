2f93b730ab3fd8faa9d38410b58f84cd4e1fdb51
JK-coder-web <jkcoder3@gmail.com>
2024-06-27 19:29:18 +0630

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import { useReducer } from "react";
+
+const firstReducer = (state, action) => {
+  switch (action.type) {
+    case "minus":
+      return { ...state, count: state.count - 1 };
+    case "plus":
+      return { ...state, count: state.count + 1 };
+    case "updateKey":
+      return { ...state, key: action.payload };
+    default:
+      throw new Error();
+  }
+};
+
+const ACTION = {
+  PLUS : "plus",
+  MINUS : "minus",
+  UPDATE_KEY : "updateKey"
+}
+
+function App() {
+  const [state, dispatch] = useReducer(firstReducer, { key: "", count: 0 });
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) =>
+          dispatch({ type: ACTION.UPDATE_KEY, payload: e.target.value })
+        }
+      />
+      <h1>Your key is - {state.key}</h1>
+      <button onClick={() => dispatch({ type: ACTION.MINUS })}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => dispatch({ type: ACTION.PLUS })}>+</button>
+    </>
+  );
+}
+
+export default App;
